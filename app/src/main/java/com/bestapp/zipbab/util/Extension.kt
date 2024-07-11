05f4adeaa78331a79576a8904d5c6f5ea855f4a1
DoDoBest <send1684@gmail.com>
2024-07-11 16:12:26 +0900

    refactor : 홈 화면 코드 수정

--- app/src/main/java/com/bestapp/zipbab/util/Extension.kt
+++ app/src/main/java/com/bestapp/zipbab/util/Extension.kt
@@ -2,7 +2,12 @@ package com.bestapp.zipbab.util
 
  import android.widget.ImageView
 import androidx.annotation.DrawableRes
-import coil.load
+ import androidx.fragment.app.Fragment
+ import androidx.fragment.app.FragmentManager
+ import androidx.navigation.NavController
+ import androidx.navigation.NavDirections
+ import androidx.navigation.fragment.findNavController
+ import coil.load
 import com.bestapp.zipbab.R
 
 fun ImageView.loadOrDefault(
@@ -16,4 +21,11 @@ fun ImageView.loadOrDefault(
     this.load(imageUrl) {
         placeholder(defaultRes)
     }
+}
+
+fun NavDirections.safeNavigate(origin: Fragment) {
+    if (origin.parentFragmentManager.fragments.last() != origin) {
+        return
+    }
+    origin.findNavController().navigate(this)
 }
