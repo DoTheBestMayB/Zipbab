99a12fa6f560fd12bb6bc1b329e644568a3fa0e0
DoTheBestMayB <send1684@gmail.com>
2024-11-26 03:54:03 +0900

    refactor: GalleryRepository 인터페이스 domain Layer로 이동

--- data/src/main/java/com/bestapp/zipbab/data/repository/GalleryRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.bestapp.zipbab.data.repository
-
-import com.bestapp.zipbab.data.local.datasource.GalleryPagingSource
-
-interface GalleryRepository {
-
-    fun galleryPagingSource(): GalleryPagingSource
-}
--- data/src/main/java/com/bestapp/zipbab/data/repository/GalleryRepositoryImpl.kt
+++ data/src/main/java/com/bestapp/zipbab/data/repository/GalleryRepositoryImpl.kt
@@ -1,13 +1,33 @@
 package com.bestapp.zipbab.data.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.map
 import com.bestapp.zipbab.data.local.datasource.GalleryImageFetcher
 import com.bestapp.zipbab.data.local.datasource.GalleryPagingSource
+import com.bestapp.zipbab.data.mapper.toGalleryImage
+import com.bestapp.zipbab.domain.model.GalleryImage
+import com.bestapp.zipbab.domain.repository.GalleryRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 internal class GalleryRepositoryImpl @Inject constructor(
     private val galleryImageFetcher: GalleryImageFetcher
-): GalleryRepository {
-    override fun galleryPagingSource(): GalleryPagingSource {
-        return GalleryPagingSource(galleryImageFetcher)
+) : GalleryRepository {
+    override fun getGalleryPagedImages(): Flow<PagingData<GalleryImage>> {
+        return Pager(
+            config = PagingConfig(pageSize = ITEMS_PER_PAGE, initialLoadSize = ITEMS_PER_PAGE * 2),
+            pagingSourceFactory = { GalleryPagingSource(galleryImageFetcher) }
+        ).flow.map { pagingData ->
+            pagingData.map { galleryImageInfo ->
+                galleryImageInfo.toGalleryImage()
+            }
+        }
+    }
+
+    companion object {
+        private const val ITEMS_PER_PAGE = 27
     }
 }
