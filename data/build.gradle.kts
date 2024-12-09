7aa42dcb6596f2534d99e96e7ac0f0525c267e1f
DoTheBestMayB <send1684@gmail.com>
2024-12-10 08:28:02 +0900

    build: room compiler ksp 의존성 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -77,6 +77,7 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.room.compiler)
+    ksp(libs.androidx.room.compiler)
 
     // Import the Firebase BoM
     implementation(platform(libs.firebase.bom))
