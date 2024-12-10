a3624c3f24242f0b17c929af36c4d1957b46ec20
DoTheBestMayB <send1684@gmail.com>
2024-12-11 01:17:47 +0900

    feat: coil interceptor를 이용해 firebase storage 이미지 캐싱 되도록 설정

--- app/src/main/java/com/bestapp/zipbab/App.kt
+++ app/src/main/java/com/bestapp/zipbab/App.kt
@@ -3,25 +3,53 @@ package com.bestapp.zipbab
 import android.app.Application
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.request.CachePolicy
+import coil.util.DebugLogger
+import com.bestapp.zipbab.util.CacheControlInterceptor
 import com.google.firebase.FirebaseApp
-import com.google.firebase.perf.metrics.AddTrace
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
 
 @HiltAndroidApp
-class App : Application(), Configuration.Provider {
+class App : Application(), Configuration.Provider, ImageLoaderFactory {
 
     @Inject lateinit var workerFactory: HiltWorkerFactory
-
-    @AddTrace(name = "onCreate")
-    override fun onCreate() {
-        super.onCreate()
-        FirebaseApp.initializeApp(applicationContext)
-    }
 
     override val workManagerConfiguration: Configuration
         get() = Configuration.Builder()
             .setWorkerFactory(workerFactory)
             .build()
+
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(applicationContext)
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader(this).newBuilder()
+            .components {
+                add(CacheControlInterceptor())
+            }
+            .memoryCachePolicy(CachePolicy.ENABLED)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.1)
+                    .strongReferencesEnabled(true)
+                    .build()
+            }
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .diskCache {
+                DiskCache.Builder()
+                    .maxSizePercent(0.03)
+                    .directory(cacheDir)
+                    .build()
+            }
+            .logger(DebugLogger())
+            .build()
+    }
 }
